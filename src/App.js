4b363057261e5aab11974e82320f01b345d378dc
reactiondeveloper <myweb0000@gmail.com>
2021-02-25 00:36:19 +0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App(){
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-4">Hello</div>
+                <div className="col-lg-4">Hello</div>
+                <div className="col-lg-4">Hello</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
